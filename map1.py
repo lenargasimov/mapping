95523bf0401c201b579856c433155459d41058a0
lenargasimov <lenargasimov@duck.com>
2022-03-03 16:25:52 +0300

    feat: added color generation function for markers

--- map1.py
+++ map1.py
@@ -6,11 +6,20 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 30000:
+        return "orange"
+    else:
+        return "red"
+
 map = folium.Map(lacation=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
  
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color=color_producer(el))))
 
 map.add_child(fg)
 
