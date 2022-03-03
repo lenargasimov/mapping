d0b78b43ad3a6b124248ac7cfabc3cc12147ac97
lenargasimov <lenargasimov@duck.com>
2022-03-03 14:19:56 +0300

    feat: added text on the Map Popup

--- map1.py
+++ map1.py
@@ -1,10 +1,16 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(lacation=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
-
-for coordinates in [[38.58, -99.1],[37.58, -98.2]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+ 
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
